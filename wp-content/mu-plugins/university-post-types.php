71026fc9a05e2eee8e18c562837e95187dbc4d91
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-09-16 09:33:35 +0000

    28. Custom Post Types (wp-content/mu-plugins/university-post-types.php)  (섹션 7: Events Post Type)

---
+++ wp-content/mu-plugins/university-post-types.php
@@ -0,0 +1,25 @@
+<?php
+function university_post_types() {
+    register_post_type('event', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new' => 'Add New Event',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+}
+  
+add_action('init', 'university_post_types');
+
+// function change_custom_post_label() {
+//     global $menu, $submenu;
+//     $post_type = 'event'; //여기에 당신이 만든 커스텀 포스트 타입 이름을 넣으세요
+//     $submenu["edit.php?post_type={$post_type}"][10][0] = 'Add New Event';
+// }
+
+// add_action( 'admin_menu', 'change_custom_post_label' );
