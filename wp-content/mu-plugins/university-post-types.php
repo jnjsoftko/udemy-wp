2aa8a3b179e7d908e81c681542b67593eede88c6
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-09-17 23:16:12 +0000

    37. Creating Relationships Between Content (single-program.php archive-program.php functions.php header.php wp-content/mu-plugins/university-post-types.php)  (섹션 8: Programs Post Type)

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,5 +1,6 @@
 <?php
 function university_post_types() {
+    // Event Post Type
     register_post_type('event', array(
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
@@ -15,6 +16,24 @@ function university_post_types() {
             'singular_name' => 'Event'
         ),
         'menu_icon' => 'dashicons-calendar'
+    ));
+
+    // Program Post Type
+    register_post_type('program', array(
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new' => 'Add New Program',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+        ),
+        'menu_icon' => 'dashicons-awards'
     ));
 }
   
