de031f3fc6fc43ca1e408d6c66fa3f4160db52d1
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-09-19 22:28:56 +0000

    64. Managing Time in JavaScript (Search.js) (섹션 13: Live Search(UI JavaScript))

--- wp-content/themes/fictional-university/src/modules/Search.js
+++ wp-content/themes/fictional-university/src/modules/Search.js
@@ -6,8 +6,10 @@ class Search {
     this.openButton = $(".js-search-trigger")
     this.closeButton = $(".search-overlay__close")
     this.searchOverlay = $(".search-overlay")
-    this.isOverlayOpen = false
+    this.searchField = $("#search-term")
     this.events()
+    this.isOverlayOpen = false
+    this.typingTimer
   }
 
   // 2. events
@@ -15,16 +17,23 @@ class Search {
     this.openButton.on("click", this.openOverlay.bind(this))
     this.closeButton.on("click", this.closeOverlay.bind(this))
     $(document).on("keydown", this.keyPressDispatcher.bind(this))
+    this.searchField.on("keydown", this.typingLogic.bind(this))
   }
 
   // 3. methods (function, action...)
+  typingLogic() {
+    clearTimeout(this.typingTimer)
+    this.typingTimer = setTimeout(function () {
+      console.log("This is a timeout test.")
+    }, 2000)
+  }
+
   keyPressDispatcher(e) {
-    // console.log(e.keyCode)
-    if (e.keyCode == 83 && !this.isOverlayOpen) {  // 's'
+    if (e.keyCode == 83 && !this.isOverlayOpen) {
       this.openOverlay()
     }
 
-    if (e.keyCode == 27 && this.isOverlayOpen) {  // 'esc'
+    if (e.keyCode == 27 && this.isOverlayOpen) {
       this.closeOverlay()
     }
   }
@@ -32,14 +41,14 @@ class Search {
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active")
     $("body").addClass("body-no-scroll")
-    console.log("our open method just ran")
+    console.log("our open method just ran!")
     this.isOverlayOpen = true
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active")
     $("body").removeClass("body-no-scroll")
-    console.log("our close method just ran")
+    console.log("our close method just ran!")
     this.isOverlayOpen = false
   }
 }
