38edf6013c8d02c9059aea12989c73a356d39f82
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-09-20 07:58:57 +0000

    79. Custom Layout & JSON based on Post Type (src/modules/Search.js inc/search-route.php) (섹션 16: Combining Front-End & Back-End)

--- wp-content/themes/fictional-university/inc/search-route.php
+++ wp-content/themes/fictional-university/inc/search-route.php
@@ -29,12 +29,15 @@ function universitySearchResults($data) {
     if (get_post_type() == 'post' OR get_post_type() == 'page') {
       array_push($results['generalInfo'], array(
         'title' => get_the_title(),
-        'permalink' => get_the_permalink()
+        'permalink' => get_the_permalink(),
+        'postType' => get_post_type(),
+        'authorName' => get_the_author()
       ));
     } else if (get_post_type() == 'professor') {
       array_push($results['professors'], array(
         'title' => get_the_title(),
-        'permalink' => get_the_permalink()
+        'permalink' => get_the_permalink(),
+        'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
       ));
     } else if (get_post_type() == 'program') {
       array_push($results['programs'], array(
@@ -47,9 +50,20 @@ function universitySearchResults($data) {
         'permalink' => get_the_permalink()
       ));
     } else if (get_post_type() == 'event') {
+      $eventDate = new DateTime(get_field('event_date'));
+      $description = null;
+      if (has_excerpt()) {
+        $description = get_the_excerpt();
+      } else {
+        $description = wp_trim_words(get_the_content(), 18);
+      }
+
       array_push($results['events'], array(
         'title' => get_the_title(),
-        'permalink' => get_the_permalink()
+        'permalink' => get_the_permalink(),
+        'month' => $eventDate->format('M'),
+        'day' => $eventDate->format('d'),
+        'description' => $description
       ));
     }
     
