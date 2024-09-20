93f3561c8b880046638de8bb5c276e5ff4a6486e
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-09-20 05:37:34 +0000

    72. Synchronous vs Asynchronous (Part 2) (footer.php Search.js) (섹션 14: WordPress REST API(AJAX))

--- wp-content/themes/fictional-university/src/modules/Search.js
+++ wp-content/themes/fictional-university/src/modules/Search.js
@@ -3,6 +3,7 @@ import $ from "jquery"
 class Search {
   // 1. describe and create/initiate our object
   constructor() {
+    this.addSearchHTML()
     this.resultsDiv = $("#search-overlay__results")
     this.openButton = $(".js-search-trigger")
     this.closeButton = $(".search-overlay__close")
@@ -33,7 +34,7 @@ class Search {
           this.resultsDiv.html('<div class="spinner-loader"></div>')
           this.isSpinnerVisible = true
         }
-        this.typingTimer = setTimeout(this.getResults.bind(this), 2000)
+        this.typingTimer = setTimeout(this.getResults.bind(this), 750)
       } else {
         this.resultsDiv.html("")
         this.isSpinnerVisible = false
@@ -44,18 +45,21 @@ class Search {
   }
 
   getResults() {
-    $.getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val(), posts => {
-      $.getJSON(universityData.root_url + "/wp-json/wp/v2/pages?search=" + this.searchField.val(), pages => {
-        var combinedResults = posts.concat(pages);
+    $.when($.getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val()), $.getJSON(universityData.root_url + "/wp-json/wp/v2/pages?search=" + this.searchField.val())).then(
+      (posts, pages) => {
+        var combinedResults = posts[0].concat(pages[0])
         this.resultsDiv.html(`
-          <h2 class="search-overlay__section-title">General Information</h2>
-          ${combinedResults.length ? '<ul class="link-list min-list">' : "<p>No general information matches that search.</p>"}
-            ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}
-          ${combinedResults.length ? "</ul>" : ""}
-        `)
+        <h2 class="search-overlay__section-title">General Information</h2>
+        ${combinedResults.length ? '<ul class="link-list min-list">' : "<p>No general information matches that search.</p>"}
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a> ${item.type == "post" ? `by ${item.authorName}` : ""}</li>`).join("")}
+        ${combinedResults.length ? "</ul>" : ""}
+      `)
         this.isSpinnerVisible = false
-      });
-    });
+      },
+      () => {
+        this.resultsDiv.html("<p>Unexpected error; please try again.</p>")
+      }
+    )
   }
 
   keyPressDispatcher(e) {
@@ -82,6 +86,25 @@ class Search {
     $("body").removeClass("body-no-scroll")
     console.log("our close method just ran!")
     this.isOverlayOpen = false
+  }
+
+  addSearchHTML() {
+    $("body").append(`
+      <div class="search-overlay">
+        <div class="search-overlay__top">
+          <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+          </div>
+        </div>
+        
+        <div class="container">
+          <div id="search-overlay__results"></div>
+        </div>
+
+      </div>
+    `)
   }
 }
 
