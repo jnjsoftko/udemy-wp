4ece0a9095e38bd57318cc8719632eb33688ae22
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-09-20 11:57:48 +0000

    83. Completing Our Search Overlay (inc/search-route.php ) (섹션 16: Combining Front-End & Back-End)

--- wp-content/themes/fictional-university/inc/search-route.php
+++ wp-content/themes/fictional-university/inc/search-route.php
@@ -40,15 +40,21 @@ function universitySearchResults($data) {
         'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
       ));
     } else if (get_post_type() == 'program') {
+      $relatedCampuses = get_field('related_campus');
+
+      if ($relatedCampuses) {
+        foreach($relatedCampuses as $campus) {
+          array_push($results['campuses'], array(
+            'title' => get_the_title($campus),
+            'permalink' => get_the_permalink($campus)
+          ));
+        }
+      }
+    
       array_push($results['programs'], array(
         'title' => get_the_title(),
         'permalink' => get_the_permalink(),
         'id' => get_the_id()
-      ));
-    } else if (get_post_type() == 'campus') {
-      array_push($results['campuses'], array(
-        'title' => get_the_title(),
-        'permalink' => get_the_permalink()
       ));
     } else if (get_post_type() == 'event') {
       $eventDate = new DateTime(get_field('event_date'));
@@ -81,13 +87,31 @@ function universitySearchResults($data) {
     }
   
     $programRelationshipQuery = new WP_Query(array(
-      'post_type' => 'professor',
+      'post_type' => array('professor', 'event'),
       'meta_query' => $programsMetaQuery
     ));
   
     while($programRelationshipQuery->have_posts()) {
       $programRelationshipQuery->the_post();
-  
+
+      if (get_post_type() == 'event') {
+        $eventDate = new DateTime(get_field('event_date'));
+        $description = null;
+        if (has_excerpt()) {
+          $description = get_the_excerpt();
+        } else {
+          $description = wp_trim_words(get_the_content(), 18);
+        }
+
+        array_push($results['events'], array(
+          'title' => get_the_title(),
+          'permalink' => get_the_permalink(),
+          'month' => $eventDate->format('M'),
+          'day' => $eventDate->format('d'),
+          'description' => $description
+        ));
+      }  
+      
       if (get_post_type() == 'professor') {
         array_push($results['professors'], array(
           'title' => get_the_title(),
@@ -98,6 +122,7 @@ function universitySearchResults($data) {
     }
   
     $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+    $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
   }
 
   return $results;
