91f9f5ce21fb9128aba047ac2188dd265445be89
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-09-20 22:17:36 +0000

    87. User Roles and Permissions (wp-content/mu-plugins/university-post-types.php) (섹션 18: NUser Roles and Permissions)

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -2,6 +2,8 @@
 function university_post_types() {
   // Campus Post Type
   register_post_type('campus', array(
+    'capability_type' => 'campus',
+    'map_meta_cap' => true,
     'show_in_rest' => true,
     'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug' => 'campuses'),
@@ -20,6 +22,8 @@ function university_post_types() {
 
     // Event Post Type
     register_post_type('event', array(
+        'capability_type' => 'event',
+        'map_meta_cap' => true,
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
         'public' => true,
