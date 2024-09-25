dedc0d0371632697784a96507c490496e41e0cbb
Jnj Soft Ko <jnjsoft.ko@gmail.com>
2024-09-25 12:41:34 +0000

    163. [2] Understanding The Pros and Cons of the "Post" Paradigm (wp-content/plugins/pets-custom-post-type/pets-custom-post-type.php`) (섹션 27: Plugin Development: Custom SQL Database Table)

--- wp-content/plugins/new-database-table/new-database-table.php
+++ wp-content/plugins/new-database-table/new-database-table.php
@@ -12,18 +12,34 @@ require_once plugin_dir_path(__FILE__) . 'inc/generatePet.php';
 
 class PetAdoptionTablePlugin {
   function __construct() {
+    global $wpdb;
+    $this->charset = $wpdb->get_charset_collate();
+    $this->tablename = $wpdb->prefix . "pets";
+
     add_action('activate_new-database-table/new-database-table.php', array($this, 'onActivate'));
-    add_action('admin_head', array($this, 'onAdminRefresh'));
+    // add_action('admin_head', array($this, 'populateFast'));
     add_action('wp_enqueue_scripts', array($this, 'loadAssets'));
     add_filter('template_include', array($this, 'loadTemplate'), 99);
   }
 
   function onActivate() {
-
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta("CREATE TABLE $this->tablename (
+      id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+      birthyear smallint(5) NOT NULL DEFAULT 0,
+      petweight smallint(5) NOT NULL DEFAULT 0,
+      favfood varchar(60) NOT NULL DEFAULT '',
+      favhobby varchar(60) NOT NULL DEFAULT '',
+      favcolor varchar(60) NOT NULL DEFAULT '',
+      petname varchar(60) NOT NULL DEFAULT '',
+      species varchar(60) NOT NULL DEFAULT '',
+      PRIMARY KEY  (id)
+    ) $this->charset;");
   }
 
   function onAdminRefresh() {
-    
+    global $wpdb;
+    $wpdb->insert($this->tablename, generatePet());
   }
 
   function loadAssets() {
